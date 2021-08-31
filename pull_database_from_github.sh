c8ecf9d97847b68e55fbb7f1c2362390716040ae
root <1kingjms@gmail.com>
2021-08-31 22:30:47 +0000

    Renamed easy update to be more accurate

---
+++ pull_database_from_github.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Warning: running this file will replace the current database with the one on github. Are you sure you wish to continue?
+select yn in "Yes" "No"; do
+    case $yn in 
+        Yes ) git pull && cp -R ./database/SmallProject /var/lib/mysql/SmallProject; break;;
+        No ) exit;;
+    esac
+done
