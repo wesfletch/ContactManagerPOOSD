33a4beb9ee1750b006f0b40e0a2fdbd88281cb2a
Mahlon Scott <1kingjms@gmail.com>
2021-08-31 19:41:06 -0400

    Debugging API

--- html/LAMPAPI/Login.php
+++ html/LAMPAPI/Login.php
@@ -14,7 +14,7 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Email='COshodi' AND Password='5832a71366768098cceb7095efb774f2'");
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Email='COshodi' AND Password='5832a71366768098cceb7095efb774f2';");
 		//$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
