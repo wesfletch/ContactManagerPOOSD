7d0eab8658fa52f045576479a6ee7469a457d5be
root <1kingjms@gmail.com>
2021-09-01 00:19:52 +0000

    Fixed missing argument in script

--- pull_database_from_github.sh
+++ pull_database_from_github.sh
@@ -2,7 +2,7 @@
 echo Warning: running this file will replace the current database with the one on github. Are you sure you wish to continue?
 select yn in "Yes" "No"; do
     case $yn in 
-        Yes ) git pull && rm /var/lib/mysql/SmallProject && cp -R ./database/SmallProject /var/lib/mysql/SmallProject; break;;
+        Yes ) git pull && rm -r /var/lib/mysql/SmallProject && cp -R ./database/SmallProject /var/lib/mysql/SmallProject; break;;
         No ) exit;;
     esac
 done
