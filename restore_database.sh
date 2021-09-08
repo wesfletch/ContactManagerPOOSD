146de7333dc2c0fcfb6ee5ab79ff549036e9ac70
Mahlon Scott <1kingjms@gmail.com>
2021-09-08 13:57:31 -0400

    Renamed to restore_database, clarified purpose

--- restore_database.sh
+++ restore_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Warning: running this file will replace the current database with the one on github. Are you sure you wish to continue?
+echo Warning: running this file will corrupt the production database. This file should only be used to restore a backup due to a major issue. Are you sure you wish to continue?
 select yn in "Yes" "No"; do
     case $yn in 
         Yes ) git pull && rm -r /var/lib/mysql/SmallProject && cp -R ./database/SmallProject /var/lib/mysql/SmallProject && chown -hR mysql:mysql /var/lib/mysql/SmallProject; break;;
