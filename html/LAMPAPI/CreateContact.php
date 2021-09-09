aecc429b3c66b161e1b7e42f750ce52910fea18a
Mahlon Scott <1kingjms@gmail.com>
2021-09-08 22:32:54 -0400

    Finished API (#4)
    * Fixed server url
    
    * Made documentation for CreateContact consistent with what it actually does
    
    * fixed documentation for CreateContact again
    
    * Fixed Error return in CreateContact
    
    * Created Update Contact endpoint
    
    * debugging UpdateContact...
    
    * debugging UpdateContact...
    
    * debugging UpdateContact...
    
    * debugging UpdateContact...
    
    * debugging UpdateContact...
    
    * debugging UpdateContact...
    
    * Fixed variable name issue in UpdateContact
    
    * Fixed field mix up during contact update
    
    * Added DeleteContact endpoint
    
    * Added GetContacts endpoint
    
    * Added documentation for C.R.U.D. endpoints
    
    * Added specs for search endpoint
    
    * Added SearchContacts endpoint
    
    * Fixing SearchContacts issue
    
    * Fixing SearchContacts issue

--- html/LAMPAPI/CreateContact.php
+++ html/LAMPAPI/CreateContact.php
@@ -17,22 +17,6 @@
     }
     else
     {
-        // checking if contact with same first name and last name exists
-        // create statement; ? will be assigned with bind_param
-        $stmt = $conn->prepare('SELECT * FROM Contacts WHERE userID=? and FirstName=? and LastName=?;');
-        // place ID from request in statement
-        $stmt->bind_param("iss", $owner, $firstName, $lastName);
-
-        // execute the statement and get result
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ( $row = $result->fetch_assoc()  )
-        {
-            returnWithError("Error creating contact; contact with name {$firstName} {$lastName} already exists", );
-        }
-        $stmt->close();
-
         // check if email is already associated with a contact
         $stmt = $conn->prepare('SELECT * FROM Contacts WHERE userID=? and Email=?');
         $stmt->bind_param("is", $owner, $email);
@@ -40,21 +24,11 @@
         $result = $stmt->get_result();
         if ( $row = $result->fetch_assoc()  )
         {
-            returnWithError("Error creating contact; contact with email %s already exists", $email);
+            returnWithError(sprintf("Error creating contact; contact with email %s already exists", $email));
         }
         $stmt->close();
 
-        // check if phone number is already associated with a contact
-        $stmt = $conn->prepare('SELECT * FROM Contacts WHERE userID=? and Phone=?');
-        $stmt->bind_param("is", $owner, $phone);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if ( $row = $result->fetch_assoc()  )
-        {
-            returnWithError("Error creating contact; contact with phone number %s already exists", $phone);
-        }
-        $stmt->close();
-
+        
         // create the new contact
         $newStmt = $conn->prepare('INSERT INTO Contacts (userID, LastName, FirstName, Email, Phone) VALUES(?, ?, ?, ?, ?)');
         $newStmt->bind_param("issss", $owner, $lastName, $firstName, $email, $phone);
@@ -66,7 +40,6 @@
         else
         {
             $newStmt->close();
-            returnWithInfo("Query: userID: {$owner}, LastName: {$lastName}, FirstName: {$firstName}, email: {$email}, phone: {$phone}", 0);
             returnWithError("Contact creation failed.");
         }
         
@@ -91,7 +64,7 @@
     
     function returnWithInfo( $err, $data)
     {
-        $retValue = '{"result":"' . $err . '", "id": ' . $data . '}';
+        $retValue = '{"result":"' . $err . '"}';
         sendResultInfoAsJson( $retValue );
     }
 
