ff2b825faaae46181eca4933837f6adfdce07674
Mahlon Scott <1kingjms@gmail.com>
2021-09-08 22:15:39 -0400

    Added SearchContacts endpoint

---
+++ html/LAMPAPI/SearchContacts.php
@@ -0,0 +1,35 @@
+<?php
+    
+    // Get query parameter
+    $userID = $_GET["userID"];
+    $pattern = $_GET["pattern"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4431", "SmallProject");
+    if( $conn->connect_error )
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        echo "{}";
+    }
+    else
+    {
+        // find any contacts associated with a userID
+        $stmt = $conn->prepare('SELECT * FROM Contacts WHERE ? IN(LastName, FirstName, Email, Phone) AND userID=?;');
+        $stmt->bind_param("si", $pattern, $userID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $out_array = "[";
+        while ($row = $result->fetch_assoc()) {
+            $out_array = sprintf("%s%s,", $out_array, json_encode($row));
+        }
+        $out_array = sprintf("%s]", $out_array);
+        sendResultInfoAsJson($out_array);
+        $stmt->close();
+
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+?>
