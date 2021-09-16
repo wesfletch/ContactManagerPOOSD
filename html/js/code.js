d17c22eb018f2a58a36a92b59d9afed155f144c2
Gregory711 <89822791+Gregory711@users.noreply.github.com>
2021-09-16 00:21:32 -0400

    Testing

--- html/js/code.js
+++ html/js/code.js
@@ -167,6 +167,8 @@ function getContacts()
       {
 	//document.getElementById("contactsError").innerHTML = "http://143.198.116.115/LAMPAPI" + endpoint + "?userID=" + userId + this.responseText;  
         var jsonArray = this.responseText;
+	document.getElementById("contactSelect").append("testing");
+	document.getElementById("contactSelect").multiselect("rebuild");
 	document.getElementById("contactsError").innerHTML = jsonArray;
         return jsonArray;
       }
