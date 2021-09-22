f2caa34b1c427c6c13bb8cc3628f862f814ff844
Rashelle A <59939664+razaula@users.noreply.github.com>
2021-09-21 22:05:29 -0400

    updated glyicon syntax

--- html/js/code.js
+++ html/js/code.js
@@ -172,7 +172,7 @@ function getContacts()
 	{
 		var jsonObject = jsonArray[i];
 		$("#contactSelect").append('<tr><td>' + jsonObject.FirstName + '</td><td>' + jsonObject.LastName + '</td><td>' + jsonObject.Email + '</td><td>' + jsonObject.Phone + 
-					   '</td><td>' + '<buttontype="button" onclick="" class="btn btn-default">' + '<span class="glyphicon glyphicon-remove" />' + '</button>' + '</td></tr>');
+					   '</td><td>' + "<button type='button' onclick=''  class='btn btn-default'>" +"<span class='glyphicon glyphicon-remove' />"+ '</td></tr>');
 	}
         return;
       }
