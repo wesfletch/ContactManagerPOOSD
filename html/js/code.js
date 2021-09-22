82c255e2ea19fd11688ca0ea6e12b5ca668268d9
Rashelle A <59939664+razaula@users.noreply.github.com>
2021-09-21 23:03:50 -0400

    reverted back to old code

--- html/js/code.js
+++ html/js/code.js
@@ -171,8 +171,7 @@ function getContacts()
 	for (var i = 0; i < jsonArray.length; i++)
 	{
 		var jsonObject = jsonArray[i];
-		$("#contactSelect").append('<tr><td>' + jsonObject.FirstName + '</td><td>' + jsonObject.LastName + '</td><td>' + jsonObject.Email + '</td><td>' + jsonObject.Phone +  
-					   '</td><td>' + <a onclick="">Delete</a> + '</td></tr>');
+		$("#contactSelect").append('<tr><td>' + jsonObject.FirstName + '</td><td>' + jsonObject.LastName + '</td><td>' + jsonObject.Email + '</td><td>' + jsonObject.Phone + '</td></tr>');
 	}
         return;
       }
