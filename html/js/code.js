d9cbcbe14589a88460c682f798813c75c44942b5
Gregory Travis <89822791+Gregory711@users.noreply.github.com>
2021-09-23 23:32:41 -0400

    Updated cookies

--- html/js/code.js
+++ html/js/code.js
@@ -140,15 +140,47 @@ function doRegister()
 
 function saveCookie()
 {
-  var minutes = 20;
-	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId;
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++)
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+    sessionStorage.setItem("userId",userId);
+    window.location.href = "contacts.html";
+	}
 }
 
 function doLogout()
 {
 	userId = 0;
+  sessionStorage.setItem("userId",userId);
 	firstName = "";
 	lastName = "";
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
